cf6d16c38bd17a0b067efd4df1e4b01e052261ec
omar2209 cr7 <omardambrogi34@gmail.com>
2025-05-03 15:50:46 +0000

    punto3

---
+++ RedditLikeApplication/src/app/email.model.ts
@@ -0,0 +1,11 @@
+class Email {
+    mail: String
+    title: String
+    text: String
+    constructor(mail: String, title: String, text: String){
+        this.mail=mail
+        this.title=title
+        this.text=text
+
+    }
+}
